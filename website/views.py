3a11954fe6e5937bc7dac4e94dc3486ff5d283e5
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2021-10-21 19:41:13 -0300

    Criação dá view cadastro de funcionario.

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView
 from helloworld.models import Funcionario
+from website.forms import InsereFuncionarioForm
 
 
 # PÁGINA PRINCIPAL
@@ -14,3 +16,12 @@ class FuncionarioListView(ListView):
     template_name = "website/lista.html"
     model = Funcionario
     context_object_name = "funcionarios"
+
+
+# CADASTRAMENTO DE FUNCIONÁRIOS
+# ----------------------------------------------
+class FuncionarioCreateView(CreateView):
+    template_name = "website/cadastra.html"
+    model = Funcionario
+    form_class = InsereFuncionarioForm
+    success_url = reverse_lazy("website:lista_funcionarios")
